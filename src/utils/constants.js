a1bcfc434082084f37adea90c3e610b4fec74d8b
Arya Shahi <aryashahi2002@gmail.com>
2023-07-31 06:56:15 +0530

    made changes to constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
-export const ACCESS_KEY = 'pwBRtByEhLs9zwNkMwGUdfEJEB3_qSFJoASzb2j1FHQ';
 export const API_URL = 'https://api.unsplash.com/photos/random';
-export const SECRET_KEY = 'O4WpvuXt4n8qNPjrI-eorHt39WWuxGTvsLwnO0wxdbo';
 export const REDIRECT_URI = 'http://localhost:3000/feeds'
